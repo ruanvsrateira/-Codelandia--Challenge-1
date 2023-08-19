153028455245b186b2801c24054f0fc22fa0a218
ruanvsrateira <vruan85@gmail.com>
2023-08-19 12:39:19 -0300

    chore(tailwind): config theme of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'lexendDeca': 
+        ['LexendDeca', 'sans-serif']
+      },
+      colors: {
+        'brand': '#574AE8',
+        'dark-10': '#1A202C',
+        'dark-20': '#717171',
+        'dark-30': '#F3F5F7',
+        'dark-40': '#FFFFFF',
+        'dark-50': '#1A202C',
+        'support-1': '#2962FF',
+        'support-2': '#0BB07B',
+        'support-3': '#FFCE52',
+        'support-4': '#F03D3D',
+        'support-5': '#E0E0E0',
+      }
+    },
+  },
+  plugins: [],
+}
+
