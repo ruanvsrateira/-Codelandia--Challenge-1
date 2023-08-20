d31c1e2fedef5a896af4d1b51f641f28839f86e2
ruanvsrateira <vruan85@gmail.com>
2023-08-20 11:24:22 -0300

    feat:  effect of hover mouse and click on heart

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,5 +1,6 @@
-import { FaRegHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import styles from "./Post.module.scss";
+import { useState } from "react";
 
 interface PostProps {
   title: string;
@@ -8,11 +9,25 @@ interface PostProps {
 }
 
 export const Post = ({ title, description, date }: PostProps) => {
+  const [heartFilled, setHeartFilled] = useState(false);
+
   return (
     <div className={styles["post-group"]}>
       <div className={styles["post-group__headerline"]}>
         <p className={styles["post-group__date"]}>{date}</p>
-        <FaRegHeart size={18} color="#574AE8" />
+        {heartFilled ? (
+          <FaHeart
+            size={18}
+            color="#574AE8"
+            onClick={() => setHeartFilled(!heartFilled)}
+          />
+        ) : (
+          <FaRegHeart
+            size={18}
+            color="#574AE8"
+            onClick={() => setHeartFilled(!heartFilled)}
+          />
+        )}
       </div>
       <h2 className={styles["post-group__title"]}>{title}</h2>
       <h3 className={styles["post-group__description"]}>{description}</h3>
