7e1f5e6ee52bdbbb77a1349743a55e6dee9c314b
ruanvsrateira <vruan85@gmail.com>
2023-08-20 11:24:48 -0300

    refactor: organizated separated local of mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./App.module.scss";
 import { Fragment } from "react";
 import { Post, SearchInput } from "./components";
+import { posts } from "./data";
 
 function App() {
   return (
@@ -17,26 +18,9 @@ function App() {
 
       <section className={styles["section-post-group"]}>
         <div className={styles["section-post-group__container"]}>
-          <Post
-            title="Agora é oficial: o Windows 11 está vindo"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. "
-            date="02 de jul, 2021"
-          />
-          <Post
-            title="Tim Berners-Lee vai leiloar código-fonte da web"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero."
-            date="02 de jul, 2021"
-          />
-          <Post
-            title="Tem Firefox novo no pedaço e você vai querer migrar"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est."
-            date="02 de jul, 2021"
-          />
-          <Post
-            title="John McAfee, criador do antivírus McAfee, morre"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero."
-            date="02 de jul, 2021"
-          />
+          {posts.map(({ title, description, date }) => {
+            return <Post title={title} description={description} date={date} />;
+          })}
         </div>
       </section>
     </Fragment>
